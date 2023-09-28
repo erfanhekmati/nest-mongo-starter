18ba01c2cd7adabc3b20cc49be7a031e74f269a6
Erfan Hekmati <erfanhekmati8@gmail.com>
2023-09-28 05:42:07 -0700

    Config module is added.

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
 
 async function bootstrap() {
   // Creating Application
   const app = await NestFactory.create(AppModule);
 
-  // Listening to a port
-  await app.listen(3000);
+  // Get configuration service from app
+  const configService = app.get(ConfigService);
+
+  // Listening on retrieved port
+  await app.listen(configService.get("app.port"));
 
   // Logging listening message
-  Logger.log(`App is listening on port ${3000} ...`, 'Bootstrap')
+  Logger.log(`App is listening on port ${configService.get("app.port")} ...`, 'Bootstrap');
 }
 bootstrap();
